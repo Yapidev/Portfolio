2da5f778ba362889f931824d23bd6ce340bee753
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-08-18 11:38:53 +0700

    update: skills section

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,58 +1,55 @@
-"use client";
-import React from "react";
-import Title from "./Title";
-import { HoverEffect } from "@/components/ui/card-hover-effect";
+"use client"
+import React from "react"
+import Title from "./Title"
+import { HoverEffect } from "@/components/ui/card-hover-effect"
 import {
-  SiCss3,
-  SiHtml5,
-  SiJavascript,
-  SiJquery,
-  SiLaravel,
-  SiNextdotjs,
-  SiPhp,
-  SiReact,
-  SiTailwindcss,
-  SiLivewire,
-  SiAlpinedotjs,
-  SiLaragon,
-  SiBootstrap,
-} from "react-icons/si";
+    SiCss3,
+    SiHtml5,
+    SiJavascript,
+    SiJquery,
+    SiLaravel,
+    SiNextdotjs,
+    SiPhp,
+    SiReact,
+    SiTailwindcss,
+    SiLivewire,
+    SiAlpinedotjs,
+    SiLaragon,
+    SiBootstrap,
+} from "react-icons/si"
 
 export default function Skills() {
-  const skills = [
-    {
-      text: "Laravel",
-      Icon: SiLaravel,
-    },
-    {
-      text: "Alpine.js",
-      Icon: SiAlpinedotjs,
-    },
-    {
-      text: "Livewire",
-      Icon: SiLivewire,
-    },
-    {
-      text: "Laragon",
-      Icon: SiLaragon,
-    },
-    {
-      text: "Boostrap",
-      Icon: SiBootstrap,
-    },
-    {
-      text: "Tailwindcss",
-      Icon: SiTailwindcss,
-    },
-  ];
+    const skills = [
+        {
+            text: "Laravel",
+            Icon: SiLaravel,
+        },
+        {
+            text: "Alpine.js",
+            Icon: SiAlpinedotjs,
+        },
+        {
+            text: "Livewire",
+            Icon: SiLivewire,
+        },
+        {
+            text: "Laragon",
+            Icon: SiLaragon,
+        },
+        {
+            text: "Boostrap",
+            Icon: SiBootstrap,
+        },
+        {
+            text: "Tailwindcss",
+            Icon: SiTailwindcss,
+        },
+    ]
 
-  return (
-    <div className="animate-move-up">
-      <Title
-        text="Skills 🎯"
-        className="flex flex-col items-center justify-center -rotate-6"
-      />
-      <HoverEffect items={skills} />
-    </div>
-  );
+    return (
+        <div className="animate-move-up">
+            <Title text="Skills 🎯" className="flex flex-col items-center justify-center -rotate-6" />
+            <HoverEffect items={skills} />
+        </div>
+    )
 }
